164e5b1215140cc805a0439b241571f8687f9f19
Pascal Brandt <psbrandt@gmail.com>
2017-04-30 17:27:32 -0700

    ⏱ Fix backup sort order.

--- api/controllers/backup.js
+++ api/controllers/backup.js
@@ -26,6 +26,10 @@ module.exports = {
       })
     })
 
+    database.sort((a, b) => {
+      return new Date(b.created).getTime() - new Date(a.created).getTime();
+    });
+
     res.json({
       database
     });
